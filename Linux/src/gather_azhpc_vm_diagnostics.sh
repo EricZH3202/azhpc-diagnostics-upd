b2d7b6f35ff3701c5e03150705e0e096b561a3ab
EricZH3202 <123799776+EricZH3202@users.noreply.github.com>
2024-08-05 14:55:46 +0800

    Update gather_azhpc_vm_diagnostics.sh
    h100

--- Linux/src/gather_azhpc_vm_diagnostics.sh
+++ Linux/src/gather_azhpc_vm_diagnostics.sh
@@ -195,6 +195,7 @@ is_nvidia_sku() {
     [[ "$clean" =~ ^standard_nd96am?sr(_a100)?_v4$ ]] ||
     [[ "$clean" =~ ^standard_nc(24|48|96)ads(_a100)?_v4$ ]] ||
     [[ "$clean" =~ ^standard_nc96ads_a100_v4$ ]] ||
+    [[ "$clean" =~ ^standard_nd96isr_h100_v5$ ]]
     \
     [[ "$clean" =~ ^standard_nv(6|12|24)$ ]] ||
     [[ "$clean" =~ ^standard_nv(6|12|24)s_v2$ ]] ||
